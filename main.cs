9f73617f0efeee93f802b2260d4c9dd91fc8dc92
Brookemooreeeee <brookemooreeeee@gmail.com>
2024-05-26 01:02:43 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program 
+{
+  public static void Main () 
+  {
+    //This sets variable name for contact and console out
+    string name = "Lucinda Potter";
+    Console.WriteLine("Name: " + name);
+
+    //This sets variable birthdate for contact and console out
+    string birthDate = "6/24/1992";
+    Console.WriteLine("Birthdate: " + birthDate);
+
+    //This sets variable workNumber for contact and console out
+    string workNumber = "000-000-0101";
+    Console.WriteLine("Work: " + workNumber);
+
+    //This sets variable cellNumber for contact and console
+    string cellNumber = "000-000-0189";
+    Console.WriteLine("Cell: " + cellNumber);
+  }
+}
