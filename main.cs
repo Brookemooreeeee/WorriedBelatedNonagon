1345544e1dca78a3bb5c91debf23f551cdbfd5a6
Brookemooreeeee <brookemooreeeee@gmail.com>
2024-05-26 01:13:44 +0000

    Unit Test #2 Commit

--- main.cs
+++ main.cs
@@ -5,19 +5,19 @@ class Program
   public static void Main () 
   {
     //This sets variable name for contact and console out
-    string name = "Lucinda Potter";
+    string name = "John Smith";
     Console.WriteLine("Name: " + name);
 
     //This sets variable birthdate for contact and console out
-    string birthDate = "6/24/1992";
+    string birthDate = "8/15/1978";
     Console.WriteLine("Birthdate: " + birthDate);
 
     //This sets variable workNumber for contact and console out
-    string workNumber = "000-000-0101";
+    string workNumber = "000-111-0101";
     Console.WriteLine("Work: " + workNumber);
 
     //This sets variable cellNumber for contact and console
-    string cellNumber = "000-000-0189";
+    string cellNumber = "000-111-0189";
     Console.WriteLine("Cell: " + cellNumber);
   }
 }
